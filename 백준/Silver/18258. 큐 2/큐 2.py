1915b98f31ef4f4bdec33879f9a1456875b84d63
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-03-14 10:42:07 +0900

    [Silver IV] Title: 큐 2, Time: 1692 ms, Memory: 175112 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,56 @@
+# 큐 2
+import sys
+import collections
+
+queue = collections.deque([])
+
+def push(x):
+    global queue
+    queue.append(x)
+
+def pop():
+    global queue
+    if len(queue) > 0:
+        print(queue[0])
+        queue.popleft()
+    else:
+        print(-1)
+
+def size():
+    global queue
+    print(len(queue))
+
+def empty():
+    global queue
+    if len(queue) > 0:
+        print(0)
+    else:
+        print(1)
+
+def front():
+    if len(queue) > 0:
+        print(queue[0])
+    else:
+        print(-1)
+
+def back():
+    if len(queue) > 0:
+        print(queue[-1])
+    else:
+        print(-1)
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    input = sys.stdin.readline().split()
+    if input[0] == 'push':
+        push(input[1])
+    elif input[0] == 'pop':
+        pop()
+    elif input[0] == 'size':
+        size()
+    elif input[0] == 'empty':
+        empty()
+    elif input[0] == 'front':
+        front()
+    elif input[0] == 'back':
+        back()
