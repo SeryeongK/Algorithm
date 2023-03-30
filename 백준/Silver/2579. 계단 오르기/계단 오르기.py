124b3eb85f104823b47cd4248d187acda77e49d2
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-03-30 10:40:57 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,23 +1,20 @@
-# 계단 오르기 - DP <== 좀이따 보자...
-"""
-앞에서부터 생각하지 말고
-마지막 칸은 무조건 밟아야 하기 때문에 🚨 마지막 칸을 기준으로 생각하기
-"""
+# 계단 오르기 - DP
 import sys
 
 N = int(sys.stdin.readline())
-steps = [0]
+stairs = []
 for _ in range(N):
-    steps.append(int(sys.stdin.readline()))
-m = [0 for _ in range(N+1)]
-for i in range(1, N+1):
-    if i == 1:
-        m[i] = steps[1]
+    stairs.append(int(sys.stdin.readline()))
+
+DP = [0 for _ in range(N)] # 시작 계단은 0번 계단
+for i in range(N):
+    if i == 0:
+        DP[i] = stairs[i] # 0번째 계단
+    elif i == 1:
+        DP[i] = stairs[i-1] + stairs[i] # 0, 1번째 계단
     elif i == 2:
-        m[i] = steps[1] + steps[2]
-    elif i == 3:
-        m[i] = max(steps[i-1]+steps[i], m[i-2]+steps[i])
+        DP[i] = max(stairs[i-2] + stairs[i], stairs[i-1] + stairs[i])
     else:
-        m[i] = max(m[i-3]+steps[i-1]+steps[i], m[i-2]+steps[i])
-# print(m)
-print(m[-1])
+        DP[i] = max(DP[i-3] + stairs[i-1] + stairs[i], DP[i-2] + stairs[i])
+
+print(DP[-1])
