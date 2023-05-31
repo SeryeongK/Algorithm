ca522f606b8fc7b2dd62faa91072a9171b59f7a4
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-05-31 14:55:43 +0900

    [Silver I] Title: 회의실 배정, Time: 296 ms, Memory: 59988 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,25 +1,20 @@
-# 회의실 배정
-"""
-아이디어 블로그 참고
-끝나는 시간을 기준으로 정렬, 가장 빠르게 끝나는 회의를 잡음
-그 회의 시간에 맞게 다음 회의 잡기
-"""
+# 회의실 배정 - 그리디
+
+# 회의 길이 기준 => 겹치는 것까지 고려해야 함 => 첫번째 회의만 잘 정하면 됨
+# 첫번째 회의 => 빨리 끝나는 회의가 결국 짧은 회의
+
 import sys
-
 N = int(sys.stdin.readline())
-meetings = []
-for _ in range(N):
-    meetings.append(list(map(int, sys.stdin.readline().split())))
+meetings = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+meetings = sorted(meetings, key=lambda x: (x[1], x[0]))
 
-## 시작 시간에 대해서도 정렬
-## 시작 시간과 끝 시간이 같은 회의가 먼저 들어왔을 때 다른 회의를 못 잡게 되는 경우 방지
-meetings.sort()
-meetings.sort(key=lambda x:x[1])
 
-timetable = [meetings[0]]
-for i in range(1, len(meetings)):
-    now = meetings[i]
-    if timetable[-1][1] <= now[0]:
-        timetable.append(meetings[i])
+# 첫번째 회의: 가장 빨리 끝나는 회의
+table = []
+table.append(meetings[0])
+if N > 1:
+    for i in range(1, N):
+        if meetings[i][0] >= table[-1][1]:
+            table.append(meetings[i])
 
-print(len(timetable))
+print(len(table))
