8d0094dc876513cf7659ca9c7075cf929315dd70
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-06-14 22:07:40 +0900

    [Silver III] Title: 1로 만들기, Time: 536 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,25 +1,22 @@
-# 1로 만들기 - DP
+# 1로 만들기
+# 원하는 정수까지의 DP 즉, 연산의 최솟값을 저장
 import sys
 N = int(sys.stdin.readline())
 
-## DP 테이블 생성
-DP = [0 for _ in range(N+1)]
-## 초기값 설정
-DP[0] = 0
-DP[1] = 0
+DP = [0] * (N+1)
+if N == 1:
+    print(0)
+else:
+    for i in range(2, N+1):
+        # -1 연산을 기본으로 둔다.
+        DP[i] = DP[i-1] + 1
 
-for i in range(2, N+1):
-    ## 숫자가 2나 3일 때(초기값)
-    if i == 2 or i == 3:
-        DP[i] = 1
-    else:   
-        if i%3 == 0 and i%2 == 0:
-            DP[i] = min(DP[i//3], DP[i//2], DP[i-1]) + 1
-        elif i%3 == 0:
-            DP[i] = min(DP[i//3], DP[i-1]) + 1
-        elif i%2 == 0:
-            DP[i] = min(DP[i//2], DP[i-1]) + 1
-        else:
-            DP[i] = DP[i-1] + 1
+        # 2로 나누어 떨어지는 경우
+        if i % 2 == 0:
+            DP[i] = min(DP[i], DP[i//2] + 1)
 
-print(DP[-1])
+        # 3으로 나누어 떨어지는 경우
+        if i % 3 == 0:
+            DP[i] = min(DP[i], DP[i//3] + 1)
+
+    print(DP[-1])
