cc182e96c6d17dc05a5a5d8a7cbbb326ef325d7d
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-06-18 20:27:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,11 +1,19 @@
 # 잃어버린 괄호
 import sys
-input = ['(']+list(sys.stdin.readline().strip())+[')']
-for i in range(len(input)):
-    if input[i] == '0':
-        if input[i-1] == '(' or input[i-1] == '' or input[i-1] == '+' or input[i-1] == '-':
-            input[i] = ''
-    # print(f"i: {i}, input[i]: {input[i]}, input: {input}")
+# -를 기준으로 분리
+input = sys.stdin.readline().strip().split("-")
+output = []
+for i in input:
+    # +를 기준으로 분리(0으로 시작하는 숫자 처리)
+    temp = i.split("+")
+    add = 0
+    for i in temp:
+        add += int(i)
+    output.append(add)
 
-st = ''.join(input)
-print(eval(st.replace('-', ')-(')))
+# -를 기준으로 분리된 숫자 더하기
+answer = output[0]
+for i in range(1, len(output)):
+    answer -= output[i]
+
+print(answer)
