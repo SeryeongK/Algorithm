87aaa3ff73b2e030e89dddf4da08da243e82d303
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-07-17 17:07:28 +0900

    [Bronze II] Title: 8진수, 10진수, 16진수, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.js
+++ 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.js
@@ -1,37 +1,14 @@
 const fs = require("fs");
 let inputData = fs.readFileSync("/dev/stdin").toString().trim();
 let answer = 0;
-if (inputData[0] == 0) {
-  if (inputData[1] == "x") {
+
+if (inputData[0] === "0") {
+  if (inputData[1] === "x") {
     // 16진수
-    let number = inputData.slice(2, inputData.length).split("");
-    for (let i = 0; i < number.length; i++) {
-      num = 0;
-      if (number[i] == "a") {
-        num = 10;
-      } else if (number[i] == "b") {
-        num = 11;
-      } else if (number[i] == "c") {
-        num = 12;
-      } else if (number[i] == "d") {
-        num = 13;
-      } else if (number[i] == "e") {
-        num = 14;
-      } else if (number[i] == "f") {
-        num = 15;
-      } else {
-        num = Number(number[i]);
-      }
-      answer += num * 16 ** (number.length - (i + 1));
-    }
-    console.log(answer);
+    console.log(parseInt(inputData, 16));
   } else {
     // 8진수
-    let number = inputData.slice(1, inputData.length).split("").map(Number);
-    for (let i = 0; i < number.length; i++) {
-      answer += number[i] * 8 ** (number.length - (i + 1));
-    }
-    console.log(answer);
+    console.log(parseInt(inputData, 8));
   }
 } else {
   // 10진수
