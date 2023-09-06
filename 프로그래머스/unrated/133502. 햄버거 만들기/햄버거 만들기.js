8021df8820c7b86ebd93ba51fa6f17c84277af10
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2023-09-06 16:02:26 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,11 +1,6 @@
 function solution(ingredient) {
-    let left = []
     let hamburger = []
-    let order = [ 1, 2, 3, 1 ]
-    let idx = 0
     let result = 0
-    let cnt = 0
-    let now = 0
     for (let i = 0; i <ingredient.length; i++) {
         hamburger.push(ingredient[i])
         let temp = hamburger.slice(-4)
