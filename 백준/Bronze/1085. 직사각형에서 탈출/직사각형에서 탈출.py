7cda6a9875b6a83800bd9b01ca1341ccf6db3635
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 12:54:31 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,18 +1,13 @@
-import sys
+x, y, w, h = map(int, input().split())
 
-input = sys.stdin.readline().split()
+min = x
 
-nums = []
+def changeMin(num):
+    global min
+    if num < min:
+        min = num
 
-def min(i):
-    x =int(i[0])
-    y = int(i[1])
-    w = int(i[2])
-    h = int(i[3])
-    nums.extend([x, y])
-    nums.append(w - x)
-    nums.append(h-y)
-    nums.sort()
-    print(nums[0])
-
-min(input)
+changeMin(y)
+changeMin(w - x)
+changeMin(h - y)
+print(min)
