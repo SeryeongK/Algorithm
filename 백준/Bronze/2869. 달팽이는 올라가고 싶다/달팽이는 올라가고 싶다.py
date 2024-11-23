bb7e924c17e861ba3f2806cde978b66681ee6c6a
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 15:43:59 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 36 ms, Memory: 33372 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,24 +1,12 @@
-# 달팽이는 올라가고 싶다
-import sys
-
-input = sys.stdin.readline().split()
-a = int(input[0])
-b = int(input[1])
-v = int(input[2])
-d = 0
-
-# 달팽이가 마지막 날에 미끄러지는 것은 상관없으니 v-b만 오르면 됨
-if (v - b) % (a - b) == 0:
-    print((v - b) // (a - b))
-else:
-    print((v - b) // (a - b) + 1)
-
-# 시간 초과 =====================
-
-# while v > 0:
-#     d += 1
-#     v -= a
-#     if v <= 0:
-#         print(d)
+import math
+a, b, v = map(int, input().split())
+# 시간 초과
+# height = 0
+# days = 0
+# while height < v:
+#     days += 1
+#     height += a
+#     if height >= v:
 #         break
-#     v += b
+#     height -= b
+print((math.ceil((v - a) / (a - b))) + 1)
