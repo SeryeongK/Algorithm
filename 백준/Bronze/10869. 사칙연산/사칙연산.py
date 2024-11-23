c0d975b344b0cec784f1a215d84c6e60c904d055
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 11:40:26 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,27 +1,7 @@
-import math
+a, b = map(int, input().split())
 
-data = input()
-num = data.split()
-a = int(num[0])
-b = int(num[1])
-
-def add(a, b):
-    return print(a + b)
-
-def minus(a, b):
-    return print(a - b)
-
-def times(a, b):
-    return print(a * b)
-
-def div(a, b):
-    return print(math.floor(a / b))
-
-def left(a, b):
-    return print(a % b)
-
-add(a, b)
-minus(a, b)
-times(a, b)
-div(a, b)
-left(a, b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a % b)
