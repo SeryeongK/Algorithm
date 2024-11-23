56b0513ee621d7e5ae0886ee1dc0cf6e038fd3d5
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 11:28:09 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,17 @@
-import sys
+input = [int(input()) for _ in range(3)]
 
-a = int(sys.stdin.readline())
-b = int(sys.stdin.readline())
-c = int(sys.stdin.readline())
+num = list(str(input[0] * input[1] * input[2]))  # list로 한글자씩 쪼개기
+numCnt = dict()
+# 숫자 채우기
+for i in range(10):
+    i = str(i)
+    if i not in numCnt:
+        numCnt[i] = 0
 
-num = a * b * c
-for n in range(10):
-    count = 0
-    for i in range(len(str(num))):
-        if str(n) == str(num)[i]:
-            count += 1
-    print(count)
+# 해당하는 숫자 카운트
+for i in num:
+    if i in numCnt:
+        numCnt[i] += 1
+
+for i in numCnt.values():
+    print(i)
