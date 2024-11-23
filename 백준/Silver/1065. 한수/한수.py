1389256487c26e5619ebdfa3e4a36e4679a1a87a
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 16:43:09 +0900

    [Silver IV] Title: 한수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,18 +1,13 @@
-# 한수
-import sys
-import math
+input = int(input())
 
-n = int(sys.stdin.readline())
-
-count = 0
-## 한수인지 아닌지 판별
-## 🚨 마지막 수까지 확인을 해야 하므로 range(n+1)
-for j in range(1, n+1):
-    j = str(j)
-    if len(j) == 1 or (len(j) ==2):
-        count += 1
-    else:
-        if (int(j[1]) - int(j[0]) == int(j[2]) - int(j[1])):
-            count += 1
-
-print(count)
+if input < 100:
+    print(input)
+elif 1000 > input >= 100:
+    cnt = 99
+    for i in range(100, input+1):
+        nums = list(map(int, list(str(i))))
+        if (nums[2] - nums[1]) == (nums[1] - nums[0]):
+            cnt += 1
+    print(cnt)
+elif input == 1000:
+    print(144)
