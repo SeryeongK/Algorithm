017cd61d0f4a03c1de310baf17d23f98a782cd6e
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 12:34:05 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,17 +1,12 @@
-import sys
+score = int(input())
 
-input = int(sys.stdin.readline())
-
-def score(s):
-    if s >= 90:
-        print("A")
-    elif s >= 80:
-        print("B")
-    elif s >= 70:
-        print("C")
-    elif s >= 60:
-        print("D")
-    else:
-        print("F")
-
-score(input)
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
