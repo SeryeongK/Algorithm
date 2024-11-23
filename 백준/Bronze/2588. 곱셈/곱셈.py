6382475a27a6b9bedfd9d57ca4b418d965ccb785
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-11-23 12:30:31 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,14 +1,8 @@
-import sys
+input = [int(input()) for _ in range(2)]
+a = input[0]
+b = str(input[1])
 
-a = int(sys.stdin.readline())
-b = sys.stdin.readline()
-b1 = int(b[0])
-b2 = int(b[1])
-b3 = int(b[2])
-
-line1 = a * b3
-line2 = a * b2
-line3 = a * b1
-final = a * int(b)
-
-print(line1, line2, line3, final ,sep="\n")
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+print(a * int(b))
