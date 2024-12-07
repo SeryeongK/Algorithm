b959877dde057c999cfeffa30c6b4fd279d30e1c
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2024-12-07 13:49:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8592 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,11 +1,16 @@
-# 수 정렬하기3 (메모리 제한 8MB)
 import sys
 
-n = int(sys.stdin.readline())
+n = int(input())
+# input을 따로 저장할 공간도 없다
+# input = [int(sys.stdin.readline()) for _ in range(n)]
 
-ls = [0] * 10001
+check = [0] * 10001
 for _ in range(n):
-    ls[int(sys.stdin.readline())] += 1
+    i = int(sys.stdin.readline())
+    check[i] += 1
+
+# dict의 경우 sort를 다시 해야 함
 for i in range(10001):
-    for n in range(ls[i]):
-        print(i)
+    if check[i] > 0:
+        for _ in range(check[i]):
+            print(i)
