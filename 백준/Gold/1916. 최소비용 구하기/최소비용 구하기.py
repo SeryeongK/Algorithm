c02d1cfead8059a5b17a306a55a4eebdd0823603
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2025-03-01 14:49:03 +0900

    [Gold V] Title: 최소비용 구하기, Time: 312 ms, Memory: 61304 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,35 +1,32 @@
-# 최소비용 구하기 <= 다익스트라 알고리즘 코드 완전 참고
 import sys
 import heapq
-
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
-input = []
-for _ in range(M):
-    input.append(list(map(int, sys.stdin.readline().split())))
-bus = [[] for _ in range(N+1)]
-for i in input:
-    a, b, v = i
-    bus[a].append([b, v])
-    ## 🚨 문제에서 출발지와 도착지라고 함 => 방향 그래프
-    # bus[b].append([a, v])
 
-distance = [float("inf")] * (N+1)
-## 책의 다익스트라 알고리즘 코드 참고
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q: # 큐가 비어있지 않다면
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in bus[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+busMap = [[] for _ in range(N + 1)]
+for _ in range(M):
+    start, end, expense = map(int, sys.stdin.readline().split())
+    busMap[start].append([end, expense])
 
 start, end = map(int, sys.stdin.readline().split())
-dijkstra(start)
-print(distance[end])
+
+def daikstra():
+    while q:
+        cost, city = heapq.heappop(q)
+        # 지금까지의 최적 비용이 이전에 city 갔을 때의 최적 비용보다 크면 더이상 가지 않고 멈춤
+        if cost > visited[city]:
+            continue
+        for end, expense in busMap[city]:
+            newExpense = visited[city] + expense
+            # 아직 가본 적 없거나 더 적은 비용으로 갈 수 있으면
+            if visited[end] > newExpense:
+                heapq.heappush(q, [newExpense, end])
+                visited[end] = newExpense
+    return
+
+INF = float('inf')
+visited = [INF] * (N + 1)
+visited[start] = 0
+q = [(0, start)]
+daikstra()
+print(visited[end])
