2c886360081ffd1bade0406a2b79fa86cc409048
Seryeong <67596636+SeryeongK@users.noreply.github.com>
2025-03-01 20:45:14 +0900

    [Gold IV] Title: 탈출, Time: 36 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -1,55 +1,64 @@
-# 탈출 - BFS
 import sys
-import collections
 R, C = map(int, sys.stdin.readline().split())
-m = []
+
+tiddup = []
 for _ in range(R):
-    m.append(list(sys.stdin.readline().strip()))
+    tiddup.append(list(sys.stdin.readline().strip()))
 
-mx = [0, 1, 0, -1]
-my = [-1, 0, 1, 0]
+waterq = []
+hedgehogq = []
+beaver = []
+for y in range(R):
+    for x in range(C):
+        loc = tiddup[y][x]
+        if loc == 'S':
+            hedgehogq.append([y, x])
+        elif loc == '*':
+            waterq.append([y, x])
+        elif loc == 'D':
+            beaver = [y, x]
 
-q = collections.deque([])
-def BFS(dx, dy):
-    while q:
-        x, y = q.popleft()
+result = 0
+
+
+def bfs(wq, hq, time):
+    global result
+    dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0, 0]
+
+    # 물 먼저, 고슴도치 나중에
+    nextWq = []
+    while wq:
+        wy, wx = wq.pop()
         for i in range(4):
-            tx = x + mx[i]
-            ty = y + my[i]
-            if 0 <= tx < R and 0 <= ty < C:
-                ## 지금이 고슴도치이고 인접 칸이 비어있거나 비버 굴이면
-                if (m[tx][ty] == "." or m[tx][ty] == "D") and m[x][y] != "*":
-                    m[tx][ty] = m[x][y] + 1
-                    if tx == dx and ty == dy:
-                        return
-                    q.append([tx, ty])
-                ## 지금이 물이고 인접 칸이 비어있거나 고슴도치(숫자)이면
-                if (m[tx][ty] == "." or str(type(m[tx][ty])) == "<class 'int'>") and m[x][y] == "*":
-                    m[tx][ty] = "*"
-                    q.append([tx, ty])
-            
-## 비버의 위치를 dx, dy로 설정
-## 고슴도치의 위치를 큐에 넣기 
-dx = 0
-dy = 0
-for i in range(R):
-    for j in range(C):
-        if m[i][j] == "D":
-            dx = i
-            dy = j
-        elif m[i][j] == "S":
-            q.append([i, j])
-            m[i][j] = 1
+            tempy = wy + dy[i]
+            tempx = wx + dx[i]
+            if 0 <= tempy < R and 0 <= tempx < C:
+                if tiddup[tempy][tempx] == ".":
+                    tiddup[tempy][tempx] = '*'
+                    nextWq.append([tempy, tempx])
 
-## 물의 위치를 큐에 넣기
-for i in range(R):
-    for j in range(C):
-        if m[i][j] == "*":
-            q.append([i, j])
+    nextHq = []
+    while hq:
+        hy, hx = hq.pop()
+        for i in range(4):
+            tempy = hy + dy[i]
+            tempx = hx + dx[i]
+            if 0 <= tempy < R and 0 <= tempx < C:
+                if tiddup[tempy][tempx] == ".":
+                    tiddup[tempy][tempx] = 'S'
+                    nextHq.append([tempy, tempx])
+                elif tiddup[tempy][tempx] == 'D':
+                    result = time
+                    return
 
-BFS(dx, dy)
-if m[dx][dy] == "D":
-    print("KAKTUS")
+    if not nextWq and not nextHq:  # 더 갈 곳이 없으면
+        return
+    bfs(nextWq, nextHq, time + 1)
+
+
+bfs(waterq, hedgehogq, 1)
+if result:
+    print(result)
 else:
-    ## 마지막에 D를 숫자로 바꿔주기 위해 한 번 더 더했으므로 1 빼기
-    print(m[dx][dy]-1)
+    print('KAKTUS')
